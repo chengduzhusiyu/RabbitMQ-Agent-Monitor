1ba462d579c364a5000382b8001330e268ce7da6
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-09-11 17:26:02 +0000

    some new stuff

--- cron/log_rotate.go
+++ cron/log_rotate.go
@@ -6,4 +6,12 @@ import (
 	"os/exec"
 
 	"github.com/barryz/rmqmonitor/g"
-	"github.com/barryz
+	"github.com/barryz/rmqmonitor/utils"
+
+	"github.com/barryz/cron"
+)
+
+func logRotateRun() {
+	log.Println("[INFO] start to rotate rabbitmq log file ......")
+	suffix := fmt.Sprintf(".%s", utils.GetYesterdayDate())
+	logRotateComman
