60c22a872af13ee6f9494c53aec20b83b500f92f
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-05-19 06:42:00 +0000

    msm: mdss: fix issue in calculating decimated dimensions

--- falcon/senddata.go
+++ falcon/senddata.go
@@ -1,1 +1,15 @@
-packa
+package falcon
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/barryz/rmqmonitor/g"
+)
+
+func sendData(data []*MetaData) (resp []byte, err error) {
+	debug := g.Config().Debug
+	js, err := json.Mars
