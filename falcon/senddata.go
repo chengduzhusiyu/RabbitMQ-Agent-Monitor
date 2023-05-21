d7f76116412d2f5d945baa73fb184d730379f3ba
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-05-21 08:49:33 +0000

    Fixed the fix

--- falcon/senddata.go
+++ falcon/senddata.go
@@ -39,4 +39,12 @@ func sendDatas(m []*MetaData) {
 		offset := lens % limit
 		for i := 0; i <= lens-1; i += limit {
 			if (i + limit - 1) >= lens {
-			
+				_, err := sendData(m[i:(offset + i - 1)])
+				if err != nil {
+					log.Println(err.Error())
+					break
+				}
+			} else {
+				_, err := sendData(m[i:(limit + i - 1)])
+				if err != nil {
+					log.Println(err.
