78b4426f4cccc61c54b6eb1c551338eb6f96952d
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-05-24 20:18:54 +0000

    adding comparison

--- funcs/funcs_test.go
+++ funcs/funcs_test.go
@@ -8,4 +8,11 @@ import (
 
 func TestGetExchanges(t *testing.T) {
 	g.ParseConfig("../config.example.yml")
-	//exchs, err 
+	//exchs, err := GetExchanges()
+	//if err != nil {
+	//	t.Error(err.Error())
+	//}
+	//for _, e := range exchs {
+	//	fmt.Printf("%s rate is %d, vhost is %s\n", e.Name, e.MsgStats.PublishIn, e.VHost)
+	//}
+}
---
+++ funcs/node.go
@@ -0,0 +1,36 @@
+
+package funcs
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/barryz/rmqmonitor/g"
+)
+
+// MemStats ...
+type MemStats struct {
+	Total              int64 `json:"total"`
+	ConnectionReaders  int64 `json:"connection_readers"`
+	ConnectionWriters  int64 `json:"connection_writers"`
+	ConnectionChannels int64 `json:"connection_channels"`
+	ConnectionOther    int64 `json:"connection_other"`
+	QueueProcs         int64 `json:"queue_procs"`
+	QueueSlaveProcs    int64 `json:"queue_slave_procs"`
+	Plugins            int64 `json:"plugins"`
+	Mnesia             int64 `json:"mnesia"`
+	MgmtDB             int64 `json:"mgmt_db"`
+	MsgIndex           int64 `json:"msg_index"`
+	Code               int64 `json:"code"`
+	Atom               int64 `json:"atom"`
+	Binary             int64 `json:"binary"`
+}
+
+// NodeStats ...
+type NodeStats struct {
+	MemStats     `json:"memory"`
+	Partitions   []string `json:"partitions"`
+	Rawait       float64  `json:"io_read_avg_time"`
+	Wawait       float64  `json:"io_write_avg_time"`
+	Syncawait    float64  `json:"io_sync_avg_time"`
