8df828eadc530b662b49c1c7950d3f777179fd66
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-09-10 13:22:56 +0000

    x86: Hyper-V: register clocksource only if its advertised

--- witch/system/launcher.go
+++ witch/system/launcher.go
@@ -1,3 +1,20 @@
 package system
 
-im
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+// Launcher supervises the process status, start, stop and restart.
+type Launcher struct {
+	pidFile string
+	cmd     string
+}
+
+// New
