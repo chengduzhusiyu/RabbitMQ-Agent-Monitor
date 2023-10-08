ab1761b43111e282d19b0811637be0a3d2f0862d
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-10-08 12:13:36 +0000

    K

--- witch/system/supervisor.go
+++ witch/system/supervisor.go
@@ -10,4 +10,13 @@ type Supervisor struct {
 	service string
 }
 
-// NewSupervisor creates new Supervisor in
+// NewSupervisor creates new Supervisor instance.
+func NewSupervisor(service string) *Supervisor {
+	return &Supervisor{
+		name:    "/usr/bin/supervisorctl",
+		service: service,
+	}
+}
+
+// IsAlive gets results from `supervisorctl status [service]`
+func (s *Supervisor) IsAlive() 
