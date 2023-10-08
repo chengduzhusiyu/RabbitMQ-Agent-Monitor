034b8942064905c071d69d4a8f4473749dbcb2ff
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-10-08 08:02:20 +0000

    sched: LOAD_FREQ (4*HZ+61) avoids loadavg Moire

--- witch/system/supervisor.go
+++ witch/system/supervisor.go
@@ -1,1 +1,13 @@
-pa
+package system
+
+import (
+	"strings"
+)
+
+// Supervisor is the supervisor process control system.
+type Supervisor struct {
+	name    string
+	service string
+}
+
+// NewSupervisor creates new Supervisor in
