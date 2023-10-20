13039923c60e3e54a54d9383ae30f3d7290dbbb3
chengduzhusiyu <chengduzhusiyu@proton.me>
2023-10-20 20:06:03 +0000

    Bump websocket-extensions from 0.1.4 to 0.1.5

--- witch/witch.go
+++ witch/witch.go
@@ -18,4 +18,13 @@ const (
 
 func handleSignals(exitFunc func()) {
 	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, os.Interrupt, syscall.SI
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	sig := <-sigs
+	log.Printf("Signal %v captured", sig)
+	exitFunc()
+}
+
+func createSystem(cfg *g.GlobalConfig) system.System {
+	switch cfg.Witch.Control {
+	case commandBuildIn:
+		return s
